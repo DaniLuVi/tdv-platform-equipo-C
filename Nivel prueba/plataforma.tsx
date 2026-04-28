f27a2d4753323acdad418d20680d1d32cd5a7db1
Minia-C <minia.cortes@edu.uah.es>
2026-04-28 12:49:22 +0200

    x

--- Nivel prueba/plataforma.tsx
+++ Nivel prueba/plataforma.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.12.1" name="plataforma" tilewidth="12" tileheight="12" tilecount="28561" columns="169">
- <image source="../Pictures/90053bc2-2077-436c-af4d-79c25f591081.jpg" width="2032" height="2032"/>
+<tileset version="1.10" tiledversion="1.12.1" name="plataforma" tilewidth="12" tileheight="12" tilecount="160" columns="10">
+ <image source="WhatsApp Image 2026-04-25 at 19.06.38.jpeg" width="128" height="192"/>
 </tileset>
