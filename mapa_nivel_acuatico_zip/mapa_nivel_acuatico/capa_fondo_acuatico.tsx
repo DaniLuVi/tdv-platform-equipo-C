7c3ee1a4329c8bad7913de070b4e0e08bbc042f4
Vachiko <minia.cortes@edu.uah.es>
2026-05-24 18:58:03 +0200

    Nivel de Rodrigo

---
+++ mapa_nivel_acuatico_zip/mapa_nivel_acuatico/capa_fondo_acuatico.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="capa_fondo_acuatico" tilewidth="64" tileheight="64" tilecount="1" columns="1">
+ <image source="capa_fondo_acuatico.jfif" width="117" height="114"/>
+</tileset>
