fddf6de81612e655544fad227a6928bcc375e7e3
Vachiko <minia.cortes@edu.uah.es>
2026-05-24 19:18:12 +0200

    Add files via upload

---
+++ mapa_nivel_acuatico_zip/puerta_chico_acuatico.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="WhatsApp Image 2026-05-14 at 16.31.45" tilewidth="64" tileheight="64" tilecount="6" columns="2">
+ <image source="WhatsApp Image 2026-05-24 at 18.54.38.jpeg" width="128" height="192"/>
+</tileset>
