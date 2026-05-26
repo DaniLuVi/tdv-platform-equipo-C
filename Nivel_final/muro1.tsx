0e92cf67c704fc6c7aa5274b798b026a660bc35b
Vachiko <minia.cortes@edu.uah.es>
2026-05-26 20:21:02 +0200

    nivel final

---
+++ Nivel_final/muro1.tsx
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="muro1" tilewidth="64" tileheight="64" tilecount="4" columns="2">
+ <image source="muro1.jpg" width="128" height="128"/>
+ <tile id="2">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+</tileset>
---
+++ Nivel_final/palancaactivada-removebg-preview.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="palancaactivada-removebg-preview" tilewidth="128" tileheight="128" tilecount="1" columns="1">
+ <image source="palancaactivada-removebg-preview.png" width="128" height="128"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="122" height="128"/>
+  </objectgroup>
+ </tile>
+</tileset>
