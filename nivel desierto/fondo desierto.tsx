d0fa96bad3d790be53ef0e78e4391ad0fd579df4
AdrianFernandez22 <adrianfergar07@gmail.com>
2026-05-27 18:12:26 +0200

    nivel desierto

---
+++ nivel desierto/fondo desierto.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="fondo desierto" tilewidth="60" tileheight="60" tilecount="130" columns="13">
+ <image source="../../../fondo desierto.webp" width="800" height="612"/>
+</tileset>
